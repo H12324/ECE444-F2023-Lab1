ec00b157871a070a510bb193fa310f5daddc255a
Hassam Sulehria <sulehria.hassam@gmail.com>
2023-09-20 00:21:30 -0400

    modified example4-7 and index.html to have an email field

--- example4-7.py
+++ example4-7.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard string to guess'
@@ -13,16 +13,23 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email()]) #Maybe add DataRequired as well
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        # For names
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you changed your name!')
         session['name'] = form.name.data
+        # For email
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you changed your email!')
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html', 
-        form=form, name=session.get('name'))
+        form=form, name=session.get('name'), email=session.get('email'))
